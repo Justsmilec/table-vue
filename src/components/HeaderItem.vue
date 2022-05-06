ab21fed1fcac147a55a72cf6ebaaa69bf06dd931
Justsmilec <gersi.coku@fti.edu.al>
2022-05-06 06:25:46 +0200

    my commit

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,58 @@
+<template>
+      <th @click="$emit('sort',title)" scope="col" class="px-6 py-3" >
+          <div class="flex flex-row ml-50">
+                <span>{{title}}</span>
+                <span>
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="svgImage"></path></svg>
+                </span>
+          </div>
+    </th>
+
+</template>
+
+<script lang="ts">
+    import { Options, Vue } from "vue-class-component";
+
+
+    @Options({
+        props:{
+            title: String,
+        }
+    })
+    export default class HeaderItem extends Vue {
+
+        title!: string;
+        
+        /*
+        if false is desc else asc
+        */
+        ascSort: any = undefined;
+        sortCount: any = 0;
+        // descSort: any = false;
+
+        svgSortingImage:any[] = [
+            "M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z",
+            "M19 14l-7 7m0 0l-7-7m7 7V3",
+            "M5 10l7-7m0 0l7 7m-7-7v18"
+        ];
+        svgImage: string = this.svgSortingImage[this.sortCount];
+
+
+        sortMethod(column: any) {            
+            if( this.ascSort == undefined ){
+                this.ascSort = true;
+                this.sortCount = 2;
+            } else{
+                this.ascSort = !this.ascSort;
+                this.sortCount = this.ascSort ? 2 : 1;    
+            }
+            this.svgImage = this.svgSortingImage[this.sortCount];
+
+        }
+        // data:string;
+    //     mounted() {
+    // }
+
+
+    }
+</script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -16,11 +16,19 @@
           "
         >
           <tr>
-            <th scope="col" class="px-6 py-3">ID</th>
-            <th scope="col" class="px-6 py-3">Name</th>
-            <th scope="col" class="px-6 py-3">Age</th>
+            <!-- <th @click="myclick" scope="col" class="px-6 py-3">ID</th> -->
+            <!-- <th scope="col" class="px-6 py-3">
+              Name <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+            </th> -->
+            <HeaderItem title="ID"/>
+            <HeaderItem @sort="sortMethod" @click.prevent="sortMethod() ref="post" title="Name"/>
+            <!-- <th scope="col" class="px-6 py-3">Age</th> -->
+            <HeaderItem title="Age"/>
+            <HeaderItem @sort="sortMethod" title="Salary"/>
+            <HeaderItem title="Company"/>
+<!-- 
             <th scope="col" class="px-6 py-3">Salary</th>
-            <th scope="col" class="px-6 py-3">Company</th>
+            <th scope="col" class="px-6 py-3">Company</th> -->
             <th scope="col" class="px-6 py-3">
               <span class="sr-only">Edit</span>
             </th>
@@ -71,14 +79,37 @@
 import { Options, Vue } from "vue-class-component";
 import { Employee } from "../models/employee";
 import { data } from "../models/data";
+import HeaderItem from "./HeaderItem.vue";
+
+@Options({
+  components: {
+    HeaderItem,
+  },
+})
 
 export default class Table extends Vue {
   msg!: string;
   dataSource: Employee[] = [];
+
   // data:string;
   mounted() {
     this.dataSource = data;
-    console.log("----------", this.dataSource);
+  }
+
+  myclick = () =>{
+    console.log("----------");
+    this.dataSource = [];
+    
+  }
+
+  sortMethod = (item: string) => {
+    console.log("-----------asdasd", item);
+    
+  }
+
+
+  toggleDetails () {
+    this.$refs.post.toggleDetails()
   }
 }
 </script>
